9317060068ad0e99492d7fdba1dfdeb4ac9b949c
eliemakodakowo <eliemakodakowo@gmail.com>
2023-10-06 02:22:13 +0200

    hey kevin juste le bout de code que tu voulais

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { withExpoSnack } from 'nativewind';
+
+import { Text, View, Button } from 'react-native';
+import { styled } from 'nativewind';
+
+const StyledView = styled(View)
+const StyledText = styled(Text)
+const Bouton=styled(Button)
+const App = () => {
+  return (
+    <StyledView className="flex-1 items-center justify-center bg-blue-600">
+      <StyledText className="text-slate-800 bg-yellow-300">
+        Try editing me! 🎉
+      </StyledText>
+      <StyledView       className='bg-zinc-600rounded pb-4'
+>
+      <Bouton
+      title='clique moi'
+      />
+      </StyledView>
+    </StyledView>
+  );
+}
+export default withExpoSnack(App);
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./**/*.{js,ts,tsx}","./src./**/*.{js,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
